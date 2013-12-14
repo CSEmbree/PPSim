154249d8060c95608dd482581a30e02c44215b44
Gradon Faulkner <gradonhf@gmail.com>
2013-12-13 23:07:20 -0800

    Time Delay Added
    Added in the time delay for the movements. Still debugging the movement
    for the GUI

--- src/ppmodel.java
+++ src/ppmodel.java
@@ -3,238 +3,243 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Random;
 
-
 public class PPModel extends SimActor {
-	//private final double DEF_PRED_SIZE = 10;
-	//private final double DEF_PREY_SIZE = 10;
+	// private final double DEF_PRED_SIZE = 10;
+	// private final double DEF_PREY_SIZE = 10;
 
-	public ArrayList<Animal> predators = new ArrayList<Animal>(); //set of all predators in simulation
-	public ArrayList<Animal> prey = new ArrayList<Animal>(); //set of all prey in simulation
-	int numPred = 0; //number of prey in the simulation currently, convenience variable
-	int numPrey = 0; //number of predators in the simulation currently, convenience variable
+	public ArrayList<Animal> predators = new ArrayList<Animal>(); // set of all
+																	// predators
+																	// in
+																	// simulation
+	public ArrayList<Animal> prey = new ArrayList<Animal>(); // set of all prey
+																// in simulation
+	int numPred = 0; // number of prey in the simulation currently, convenience
+						// variable
+	int numPrey = 0; // number of predators in the simulation currently,
+						// convenience variable
 
-	double xSize = 0.0; //max x dimention of simulation (x=0, 1,...,xSize)
-	double ySize = 0.0; //max y dimention of simulation (y=0, 1,...,ySize)
+	double xSize = 0.0; // max x dimention of simulation (x=0, 1,...,xSize)
+	double ySize = 0.0; // max y dimention of simulation (y=0, 1,...,ySize)
 
-
-	public PPModel() 
-	{
+	public PPModel() {
 		this(0, 0, 100.0, 100.0);
 	}
 
-	public PPModel(int numPred, int numPrey, double xSize, double ySize)
-	{
-		//set initial number of predators and prey
+	public PPModel(int numPred, int numPrey, double xSize, double ySize) {
+		// set initial number of predators and prey
 		this.setNumPred(numPred);
 		this.setNumPrey(numPrey);
 
-
-		//set size for field of play
-		if(xSize > 0.0)
-			this.setXSize( xSize );
+		// set size for field of play
+		if (xSize > 0.0)
+			this.setXSize(xSize);
 		else {
-			System.out.println("PPModel::PPModel: ERROR: An invalid value for xSize= "+xSize+" was enountered. xSize must be >= 1.");
-			System.exit(0); //exit gracefully
+			System.out
+					.println("PPModel::PPModel: ERROR: An invalid value for xSize= "
+							+ xSize + " was enountered. xSize must be >= 1.");
+			System.exit(0); // exit gracefully
 		}
 
-		if(ySize > 0.0)
-			this.setYSize( ySize) ;
+		if (ySize > 0.0)
+			this.setYSize(ySize);
 		else {
-			System.out.println("PPModel::PPModel: ERROR: An invalid value for ySize= "+ySize+" was enountered. xSize must be >= 1.");
-			System.exit(0); //exit gracefully
+			System.out
+					.println("PPModel::PPModel: ERROR: An invalid value for ySize= "
+							+ ySize + " was enountered. xSize must be >= 1.");
+			System.exit(0); // exit gracefully
 		}
 
 		Random rand = new Random();
 		rand.setSeed(System.currentTimeMillis());
-		
-		//int randPosX = rand.nextInt((int)xSize);
-		//int randPosY = rand.nextInt((int)ySize);
 
-		//create that number of preds and prey
+		// int randPosX = rand.nextInt((int)xSize);
+		// int randPosY = rand.nextInt((int)ySize);
+
+		// create that number of preds and prey
 		double xPos, yPos, energy, maxDistTravel;
 		for (int i = 0; i < this.numPred; i++) {
-			String predName = "shark"+i;
+			String predName = "shark" + i;
 			String predType = "predator";
-			String id = predType+"_"+predName;
+			String id = predType + "_" + predName;
 			String predSpecies = "great white";
 			energy = 100.0;
 			maxDistTravel = 20.0;
-			xPos = rand.nextInt((int)xSize);
-			yPos = rand.nextInt((int)ySize);
+			xPos = rand.nextInt((int) xSize);
+			yPos = rand.nextInt((int) ySize);
 
-			Shark predShark = new Shark( id, predName, predType, predSpecies, energy, maxDistTravel, xPos, yPos);
+			Shark predShark = new Shark(id, predName, predType, predSpecies,
+					energy, maxDistTravel, xPos, yPos);
 
-			predators.add( predShark );
+			predators.add(predShark);
 		}
 
 		for (int i = 0; i < this.numPrey; i++) {
-			String preyName = "fish"+i;
+			String preyName = "fish" + i;
 			String preyType = "prey";
-			String id = preyType+"_"+preyName;
+			String id = preyType + "_" + preyName;
 			String preySpecies = "gold fish";
 			energy = 100;
 			maxDistTravel = 10;
-			xPos = rand.nextInt((int)xSize);
-			yPos = rand.nextInt((int)ySize);
+			xPos = rand.nextInt((int) xSize);
+			yPos = rand.nextInt((int) ySize);
 
-			Fish preyFish = new Fish( id, preyName, preyType, preySpecies, energy, maxDistTravel, xPos, yPos);
+			Fish preyFish = new Fish(id, preyName, preyType, preySpecies,
+					energy, maxDistTravel, xPos, yPos);
 
-			prey.add( preyFish );
+			prey.add(preyFish);
 		}
 	}
 
+	public void runSimulation(int time, double timeStepPartitions) {
+		System.out.println("PPModel::runSimulation: Starting run for " + time
+				+ " time.");
 
-	public void runSimulation(int time, double timeStepPartitions)
-	{
-		System.out.println("PPModel::runSimulation: Starting run for " + time + " time.");
-
-
-		for(int currentTime = time; currentTime > 0; currentTime--)
-		{
-			System.out.println("PPModel::runSimulation: TIME STEP: " + currentTime);
+		for (int currentTime = time; currentTime > 0; currentTime--) {
+			System.out.println("PPModel::runSimulation: TIME STEP: "
+					+ currentTime);
 
 			cleanAndPlan(timeStepPartitions);
-			//Thread.sleep(50);
-			 try {
-                Thread.sleep(50);
-            } catch(InterruptedException e) {
-                return;
-            }
-			activateAndMove(timeStepPartitions);
-			//Thread.sleep(50);	
+
 			try {
-                Thread.sleep(50);
-            } catch(InterruptedException e) {
-                return;
-            }
+				// System.out.println("PPModel::runSimulation: CHECKING TIME DELAY TRY");
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// System.out.println("PPModel::runSimulation: CHECKING TIME DELAY CATCH");
+				return;
+			}
+			activateAndMove(timeStepPartitions);
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				return;
+			}
 		}
 	}
 
-	public void cleanAndPlan(double timeStepPartitions)
-	{
-		System.out.println("PPModel::cleanAndPlan: Cleaning updating any dead actors and deciding their next move");
+	public void cleanAndPlan(double timeStepPartitions) {
+		System.out
+				.println("PPModel::cleanAndPlan: Cleaning updating any dead actors and deciding their next move");
 		boolean removed;
 
-
-		//remove any dead predators and determine their next move
+		// remove any dead predators and determine their next move
 		removed = true;
-		while(removed == true) 
-		{
+		while (removed == true) {
 			removed = false;
-			for(int currentActorIndex = 0; currentActorIndex < predators.size(); currentActorIndex++ )
-			{
-				if(predators.get(currentActorIndex).getEnergy() <= 0)
-				{
+			for (int currentActorIndex = 0; currentActorIndex < predators
+					.size(); currentActorIndex++) {
+				if (predators.get(currentActorIndex).getEnergy() <= 0) {
 					predators.remove(currentActorIndex);
-					removed = true; //if clean up was performed, run the whole loop again
+					removed = true; // if clean up was performed, run the whole
+									// loop again
 				}
 			}
 		}
 
-
-		for(int currentActorIndex = 0; currentActorIndex < predators.size(); currentActorIndex++ )
-		{
-			predators.get(currentActorIndex).chooseNewDestination(xSize, ySize, timeStepPartitions);	
+		for (int currentActorIndex = 0; currentActorIndex < predators.size(); currentActorIndex++) {
+			predators.get(currentActorIndex).chooseNewDestination(xSize, ySize,
+					timeStepPartitions);
 		}
 
-
-
-		//remove any dead prey and determine their next move
+		// remove any dead prey and determine their next move
 		removed = true;
-		while(removed == true) 
-		{
+		while (removed == true) {
 			removed = false;
-			for(int currentActorIndex = 0; currentActorIndex < prey.size(); currentActorIndex++ )
-			{
-				if(prey.get(currentActorIndex).getEnergy() <= 0)
-				{
+			for (int currentActorIndex = 0; currentActorIndex < prey.size(); currentActorIndex++) {
+				if (prey.get(currentActorIndex).getEnergy() <= 0) {
 					prey.remove(currentActorIndex);
-					removed = true; //if clean up was performed, run the whole loop again
+					removed = true; // if clean up was performed, run the whole
+									// loop again
 				}
 			}
 		}
 
-		for(int currentActorIndex = 0; currentActorIndex < prey.size(); currentActorIndex++ )
-		{
-			prey.get(currentActorIndex).chooseNewDestination(xSize, ySize, timeStepPartitions);	
+		for (int currentActorIndex = 0; currentActorIndex < prey.size(); currentActorIndex++) {
+			prey.get(currentActorIndex).chooseNewDestination(xSize, ySize,
+					timeStepPartitions);
 		}
 	}
 
-	private void activateAndMove(double timeStepPartitions)
-	{
-		System.out.println("PPModel::activateAndMove: Starting to move things...");
+	private void activateAndMove(double timeStepPartitions) {
+		System.out
+				.println("PPModel::activateAndMove: Starting to move things...");
 
-		//TODO - account for delta move and add in steps
-		for (int i = 0; i < timeStepPartitions; i++) 
-		{
-			//System.out.println("PPModel::activateAndMove: Partial Iteration "+i+" of "+timeStepPartitions);
-			
+		// TODO - account for delta move and add in steps
+		for (int i = 0; i < timeStepPartitions; i++) {
+			// System.out.println("PPModel::activateAndMove: Partial Iteration "+i+" of "+timeStepPartitions);
+
 			for (Animal an : predators) {
-				an.setXYPosition( an.getXCoord()+an.getDeltaX(), an.getYCoord()+an.getDeltaY() );
+				an.setXYPosition(an.getXCoord() + an.getDeltaX(),
+						an.getYCoord() + an.getDeltaY());
 				setChanged();
-            	notifyObservers();
+				notifyObservers();
+				// try {
+				// 	Thread.sleep(500);
+				// } catch (InterruptedException e) {
+				// 	return;
+				// }
 			}
 
 			for (Animal an : prey) {
-				an.setXYPosition( an.getXCoord()+an.getDeltaX(), an.getYCoord()+an.getDeltaY() );
+				an.setXYPosition(an.getXCoord() + an.getDeltaX(),
+						an.getYCoord() + an.getDeltaY());
 				setChanged();
-           	 	notifyObservers();
+				notifyObservers();
+				// try {
+				// 	Thread.sleep(500);
+				// } catch (InterruptedException e) {
+				// 	return;
+				// }
 			}
-			//Thread.sleep(50);
+			// Thread.sleep(50);
 		}
-		this.displayInfo(); //for state debug checking
+		this.displayInfo(); // for state debug checking
 
-
-		System.out.println("PPModel::activateAndMove: Stopping moving things...");
+		System.out
+				.println("PPModel::activateAndMove: Stopping moving things...");
 	}
 
-	public double getXCoordSize()
-	{
+	public double getXCoordSize() {
 		return this.xSize;
 	}
 
-	public double getYCoordSize()
-	{
+	public double getYCoordSize() {
 		return this.ySize;
 	}
 
-	private void setNumPrey(int preyCount)
-	{
+	private void setNumPrey(int preyCount) {
 		this.numPrey = preyCount;
 	}
 
-	private void setNumPred(int predCount)
-	{
+	private void setNumPred(int predCount) {
 		this.numPred = predCount;
 	}
 
-	private void setXSize( double xSize )
-	{
+	private void setXSize(double xSize) {
 		this.xSize = xSize;
 	}
 
-	private void setYSize( double ySize )
-	{
+	private void setYSize(double ySize) {
 		this.ySize = ySize;
 	}
-	
-	public void displayPredInfo()
-	{
+
+	public void displayPredInfo() {
 		for (Animal an : predators) {
-			System.out.println("\tPred Name:"+an.getName()+" at position ("+an.getXCoord()+","+an.getYCoord()+")");
-		}
-	}
-	
-	public void displayPreyInfo()
-	{
-		for (Animal an : prey) {
-			System.out.println("\tPrey Name:"+an.getName()+" at position ("+an.getXCoord()+","+an.getYCoord()+")");
+			System.out.println("\tPred Name:" + an.getName() + " at position ("
+					+ an.getXCoord() + "," + an.getYCoord() + ")");
 		}
 	}
 
-	public void displayInfo()
-	{
-		System.out.println("PPModel has "+this.numPred+" Predators and "+this.numPrey+" Prey on a field of "+xSize+"x"+ySize );
+	public void displayPreyInfo() {
+		for (Animal an : prey) {
+			System.out.println("\tPrey Name:" + an.getName() + " at position ("
+					+ an.getXCoord() + "," + an.getYCoord() + ")");
+		}
+	}
+
+	public void displayInfo() {
+		System.out.println("PPModel has " + this.numPred + " Predators and "
+				+ this.numPrey + " Prey on a field of " + xSize + "x" + ySize);
 		this.displayPredInfo();
 		this.displayPreyInfo();
 
